253ae4feb21d5dbab249385d340784e6c07b2a74
emmanesguerra <emman.esguerra2013@gmail.com>
2020-08-20 16:13:14 +0800

    fix readmore link

--- resources/views/modules/news/category.blade.php
+++ resources/views/modules/news/category.blade.php
@@ -8,7 +8,7 @@
             <h3><a href='/news/{{ $new->category->slug }}/{{ $new->slug }}'>{{ $new->title }}</a></h3>
             <span class='date'>{{ date('M d Y', strtotime($new->created_at)) }} | {{ Carbon\Carbon::parse($new->created_at)->diffForHumans() }}</span>
             <p>{{ $new->short_description }}</p>
-            <span class='readmore'><a href='{{ $new->category->slug }}/{{ $new->slug }}'>Read More</a></span>
+            <span class='readmore'><a href='/news/{{ $new->category->slug }}/{{ $new->slug }}'>Read More</a></span>
         </section>
         @endforeach
     </div>
--- resources/views/modules/news/list.blade.php
+++ resources/views/modules/news/list.blade.php
@@ -3,6 +3,6 @@
         <h3><a href='/news/{{ $new->category->slug }}/{{ $new->slug }}'>{{ $new->title }}</a></h3>
         <span class='date'><a href="/news/{{ $new->category->slug }}" style="font-size: 14px;">{{ $new->category->name }}</a> | {{ date('M d Y', strtotime($new->created_at)) }} | {{ Carbon\Carbon::parse($new->created_at)->diffForHumans() }}</span>
         <p>{{ $new->short_description }}</p>
-        <span class='readmore'><a href='{{ $new->category->slug }}/{{ $new->slug }}'>Read More</a></span>
+        <span class='readmore'><a href='/news/{{ $new->category->slug }}/{{ $new->slug }}'>Read More</a></span>
     </section>
 @endforeach
