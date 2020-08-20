ce1e467bdd6d6f9c61bc85185a22ec92ec6b2a9e
emmanesguerra <emman.esguerra2013@gmail.com>
2020-08-20 12:17:14 +0800

    fix adding active class

--- resources/views/modules/news/panels/category.blade.php
+++ resources/views/modules/news/panels/category.blade.php
@@ -2,7 +2,7 @@
     <h3>News Categories</h3>
     <ul>
         @foreach($categories as $categ)
-        <li><a href="/news/{{ $categ->slug }}">{{ $categ->name }} ({{ count($categ->news) }}) </a></li>
+        <li class="{{ (strpos(Request::getPathInfo(), $categ->slug)) ? 'active' : '' }}"><a href="/news/{{ $categ->slug }}">{{ $categ->name }} ({{ count($categ->news) }}) </a></li>
         @endforeach
     </ul>
 </div>
