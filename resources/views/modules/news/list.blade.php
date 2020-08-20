da252201710e021ac0d199135fe998d18d6df1e2
emmanesguerra <emman.esguerra2013@gmail.com>
2020-08-20 09:01:29 +0800

    add news link

--- resources/views/modules/news/list.blade.php
+++ resources/views/modules/news/list.blade.php
@@ -1,6 +1,6 @@
 @foreach($news as $new)
     <section>
-        <h3><a href='{{ $new->category->slug }}/{{ $new->slug }}'>{{ $new->title }}</a></h3>
+        <h3><a href='/news/{{ $new->category->slug }}/{{ $new->slug }}'>{{ $new->title }}</a></h3>
         <span class='date'>{{ date('M d Y', strtotime($new->created_at)) }}</span>
         <p>{{ $new->short_description }}</p>
         <span class='readmore'><a href='{{ $new->category->slug }}/{{ $new->slug }}'>Read More</a></span>
