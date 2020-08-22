38e6c8b44bd86374efa9268e96e76337787433c1
emmanesguerra <emman.esguerra2013@gmail.com>
2020-08-22 18:54:18 +0800

    fix navbar

--- resources/views/templates/defaultapp.blade.php
+++ resources/views/templates/defaultapp.blade.php
@@ -22,7 +22,7 @@
             </div>
             <div class="col-sm-8 my-3">
                 <nav class="navbar navbar-expand-lg navbar-light">
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainnav" aria-controls="mainnav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div id="mainnav"  class="collapse navbar-collapse">
